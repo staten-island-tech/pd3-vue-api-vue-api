891afc4e7a5ebc29734074bebd29d1410c4a9da9
johnnylu3 <lujohnny517@gmail.com>
2023-04-07 18:39:41 -0400

    Api call

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div></div>
 </template>
+
+<script>
+import { ref, onMounted } from 'vue'
+const pokemon = ref('')
+async function getPokemon() {
+  let result = await fetch(
+    'https://data.cityofnewyork.us/resource/erm2-nwe9.json?$$app_token=YetA19ejbBFc3cdnJ207sMngx'
+  )
+  let data = await result.json()
+  pokemon.value = data
+}
+onMounted(() => {
+  getPokemon()
+})
+</script>
+<style lang="scss" scoped></style>
