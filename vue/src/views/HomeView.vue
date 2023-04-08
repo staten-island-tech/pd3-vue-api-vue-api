a8dd50e062afe635f88d2452d0db967dffecce76
Ucheong <ucheongsou@gmail.com>
2023-04-07 20:39:09 -0400

    updated names

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -4,16 +4,16 @@
 
 <script>
 import { ref, onMounted } from 'vue'
-const pokemon = ref('')
-async function getPokemon() {
+const call = ref('')
+async function getCall() {
   let result = await fetch(
     'https://data.cityofnewyork.us/resource/erm2-nwe9.json?$$app_token=YetA19ejbBFc3cdnJ207sMngx'
   )
   let data = await result.json()
-  pokemon.value = data
+  call.value = data
 }
 onMounted(() => {
-  getPokemon()
+  getCall()
 })
 </script>
-<style lang="scss" scoped></style>
+<style scoped></style>
