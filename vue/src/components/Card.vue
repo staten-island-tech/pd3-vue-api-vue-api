de2d2cae1a1b0cb1f0fe303329c515433aec98c6
Ucheong <ucheongsou@gmail.com>
2023-04-07 22:24:43 -0400

    updated props, need to make cards show api stuff

--- vue/src/components/Card.vue
+++ vue/src/components/Card.vue
@@ -1,25 +1,24 @@
 <template>
-    <div class="card">
-        <h2>{{ name }}</h2>
-        <h3>{{ id }}</h3>
-
-
-    </div>
+  <div class="card">
+    <h2>{{ complaintType }}</h2>
+    <p>{{ description }}</p>
+    <p>Called {{ agencyName }}</p>
+    <p>Address:{{ address }}</p>
+    <p>Date:{{ date }}</p>
+    <p>{{ borough }}</p>
+  </div>
 </template>
 
 <script setup>
-const 
-borough: String
-status: String
-date: String //create method for date
-address: String
-agency name: String
-complaint type: string
-description: string
-
-
+defineProps({
+  borough: String,
+  status: String,
+  date: String, //create method for date
+  address: String,
+  agencyName: String,
+  complaintType: String,
+  description: String
+})
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="container">
-  <Card></Card>
+    <Card v-for="call in calls" :key="call" :id="index + 1" :calls="call"> </Card>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import Card from "../components/Card.vue"
-const call = ref('')
+import Card from '../components/Card.vue'
+const calls = ref('')
 async function getCall() {
   let result = await fetch(
     'https://data.cityofnewyork.us/resource/erm2-nwe9.json?$$app_token=YetA19ejbBFc3cdnJ207sMngx'
   )
   let data = await result.json()
-  call.value = data
+  calls.value = data
 }
 onMounted(() => {
   getCall()
