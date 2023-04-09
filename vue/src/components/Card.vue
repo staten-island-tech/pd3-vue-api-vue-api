2f1618d8fb44c330eabff5f69c63ef3a6f7b137d
johnnylu3 <lujohnny517@gmail.com>
2023-04-08 21:38:54 -0400

    pages aded

--- vue/src/components/Card.vue
+++ vue/src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <router-link :to="path" class="card">
     <h2>{{ request.complaint_type }}</h2>
     <p>Description: {{ request.descriptor }}</p>
     <p>Called {{ request.agency_name }}, {{ request.agency }}</p>
@@ -8,14 +8,33 @@
     <p v-else>Date Created: {{ request.created_date }}</p>
     <p>Borough: {{ request.borough }}</p>
     <p>Status: {{ request.status }}</p>
-  </div>
+  </router-link>
+  <!-- updated date -->
 </template>
 
 <script setup>
-defineProps({
+import {computed} from 'vue'
+const props = defineProps({
   request: Object,
   id: Number
 })
+const path = computed(()=>{return `/Data/${props.request}`})
 </script>
 
-<style scoped></style>
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 27vw;
+  height: 65vh;
+  margin: 1rem auto;
+  align-items: center;
+  background-color: var(--secondary);
+  border: solid;
+  border-radius: 1rem;
+  border: 1px solid;
+  color: var(--inv);
+  box-shadow: var(--shadow) 0px 3px 8px;
+}
+</style>
---
+++ vue/src/views/Data.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+xd
+    </div>
+</template>
+
+<script>
+    // export default {
+    //     data() {
+    //         return {
+    //             call: {}
+    //         }
+    //     },
+    //     mounted: async function(){
+    //         await getPokemon()
+    //     },
+    //     methods: {
+    //         getPokemon
+    //     }
+    // }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
