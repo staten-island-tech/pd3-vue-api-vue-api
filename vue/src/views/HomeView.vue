ffd2673fa2f03c9d3650a6ed4170b4bbc6e74a19
johnnylu3 <lujohnny517@gmail.com>
2023-04-24 10:09:37 -0400

    Update HomeView.vue

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <template>
+<<<<<<< Updated upstream
   <div class="container">borough="call.borough"
     <Card v-for="(call, index) in calls" :key="call.unique_key" :id="index + 1" :request="call"> </Card>
+=======
+  <div class="container">
+    <Card v-for="(call, index) in calls" :key="call.unique_key" :id="index + 1" :request="call"> </Card>
+    <div class="pagination">
+      <button v-if="currentPage > 1" @click="currentPage--">Previous</button>
+      <button v-if="hasNext" @click="currentPage++">Next</button>
+    </div>
+>>>>>>> Stashed changes
   </div>
 </template>
 
@@ -8,6 +17,12 @@
 import { ref, onMounted } from 'vue'
 import Card from '../components/Card.vue'
 const calls = ref('')
+<<<<<<< Updated upstream
+=======
+let currentPage = ref(1)
+const pageSize = 12
+
+>>>>>>> Stashed changes
 async function getCall() {
   let result = await fetch(
     'https://data.cityofnewyork.us/resource/erm2-nwe9.json?$$app_token=YetA19ejbBFc3cdnJ207sMngx'
@@ -18,5 +33,12 @@ async function getCall() {
 onMounted(() => {
   getCall()
 })
+<<<<<<< Updated upstream
+=======
+
+const hasNext = computed(() => {
+  return calls.value && calls.value.length === pageSize
+})
+>>>>>>> Stashed changes
 </script>
 <style scoped></style>
