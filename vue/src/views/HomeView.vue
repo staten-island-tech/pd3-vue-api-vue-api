cd39bf210e680391338f4882526c67390430456f
johnnylu3 <lujohnny517@gmail.com>
2023-04-30 14:21:56 -0400

    done

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div>311 Calls from 2010 to the present</div>
     <div class="container">
       <Card
         v-for="(call, index) in calls"
@@ -8,29 +9,46 @@
         :request="call"
       >
       </Card>
-      </div>
     </div>
+    <div class="pagination">
+      <button v-if="currentPage > 1" @click="prevPage()">Previous</button>
+      <span>Page {{ currentPage }}</span>
+      <button v-if="calls.length === pageSize" @click="nextPage()">Next</button>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import Card from '../components/Card.vue'
 const calls = ref('')
+const currentPage = ref(1)
+const pageSize = 12
 
 async function getCall() {
+  let offset = (currentPage.value - 1) * pageSize
   let result = await fetch(
-    'https://data.cityofnewyork.us/resource/erm2-nwe9.json?$$app_token=YetA19ejbBFc3cdnJ207sMngx'
+    `https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit=${pageSize}&$offset=${offset}&$order=created_date DESC&$$app_token=YetA19ejbBFc3cdnJ207sMngx`
   )
   let data = await result.json()
   calls.value = data
 }
 
+function nextPage() {
+  currentPage.value ++ 
+  getCall()
+}
+
+function prevPage() {
+  currentPage.value --
+  getCall()
+}
+
 onMounted(() => {
   getCall()
 })
-
-
 </script>
+
 
 <style scoped>
 .container {
