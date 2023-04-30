07306ece0b57029b56dd03c72732785594f51290
johnnylu3 <lujohnny517@gmail.com>
2023-04-30 16:49:40 -0400

    LINECHART DONE

--- vue/src/components/LineChart.vue
+++ vue/src/components/LineChart.vue
@@ -5,8 +5,26 @@
 
 <script>
 import { Line } from 'vue-chartjs'
-import { Chart as ChartJS, LineElement, Tooltip, Legend } from 'chart.js'
-ChartJS.register(LineElement, Tooltip, Legend)
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js' 
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
 export default {
   name: 'LineChart',
   components: { Line },
